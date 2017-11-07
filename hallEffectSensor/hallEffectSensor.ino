d7dcb7b086f55d4caaa7952378978487465f8bbc
ljordan51 <ljordan51@gmail.com>
2017-11-07 09:19:20 -0500

    adding code to read hall effect sensor

---
+++ hallEffectSensor/hallEffectSensor.ino
@@ -0,0 +1,15 @@
+const int input = 4;
+
+void setup() {
+  // put your setup code here, to run once:
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  int sensor = digitalRead(input);
+  Serial.println(sensor);
+
+}
